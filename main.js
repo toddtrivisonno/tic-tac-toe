694115723649c81e9716c61eff5c5ccf55a2ccbf
Todd Trivisonno <todd.trivisonno@gmail.com>
2019-09-26 11:51:11 -0400

    style board and add click count logic

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
 
 document.body.onload = setBoard;
+
 var app = document.getElementById('app')
+var numberClicks = 1;
+var arr = [];
+
+function marker() {
+   console.log('functioning', numberClicks)
+   numberClicks++;
+
+
+   if (numberClicks % 2) {
+   this.innerHTML = 'O';
+   } else {
+      this.innerHTML = 'X';
+   }
+   console.log(this);
+   // arr = this;
+   // console.log(arr)
+}
 
 function setBoard() {
    // var element = document.createElement(tagName[, options]);
@@ -16,10 +34,12 @@ function setBoard() {
       var rowDiv = document.createElement('div');
       rowDiv.className = "row justify-content-center";
 
-      for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
          var colDiv = document.createElement('div');
          colDiv.className = "col-3 border btn-lg";
-         colDiv.innerHTML = "Button";
+         colDiv.id = i + "" + j;
+         colDiv.addEventListener('click', marker);
+
          rowDiv.appendChild(colDiv);
       }
       newDiv.appendChild(rowDiv);
