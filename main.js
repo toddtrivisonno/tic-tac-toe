1205c26195b07f1d3cb5c70dea09b4db9e444fd4
Todd Trivisonno <todd.trivisonno@gmail.com>
2019-09-27 09:04:36 -0400

    add reset button

--- main.js
+++ main.js
@@ -2,13 +2,11 @@
 document.body.onload = setBoard;
 
 var app = document.getElementById('app')
-var numberClicks = 1;
-var arr = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-var status = "";
+var numberClicks, arr, status;
 
 function checkWin() {
-   console.log('check if someone wins')
-   console.log(arr);
+   // console.log('check if someone wins')
+   // console.log(arr);
    var wins = [
       [0, 1, 2],
       [3, 4, 5],
@@ -19,7 +17,6 @@ function checkWin() {
       [0, 4, 8],
       [2, 4, 6]
    ]
-   console.log(typeof status);
 
    for (let i = 0; i < wins.length; i++) {
       var a = checkVal(wins[i][0], wins[i][1], wins[i][2]);
@@ -31,27 +28,27 @@ function checkWin() {
    }
    // no winner, check for tie
    if (numberClicks == 10 && status == "") {
-      alert("cat game");
+      alert("Cat game");
    }
-   
+
 }
 
 function checkVal(a, b, c) {
    // if any == 0 return 0
-   if(arr[a] == 0 || arr[b] == 0 || arr[c] == 0){
+   if (arr[a] == 0 || arr[b] == 0 || arr[c] == 0) {
       return 0;
    }
    // if the sum of a b c == 3 return 1
    var sum = arr[a] + arr[b] + arr[c];
-   if(sum == 3){
+   if (sum == 3) {
       return 1;
    }
-   if(sum == 6){
+   if (sum == 6) {
       return 2;
    }
    return 0;
-   //  -----   a b c = 6 return 2
 }
+
 
 function marker() {
    // console.log('functioning', numberClicks)
@@ -61,9 +58,11 @@ function marker() {
       numberClicks++;
       if (numberClicks % 2) {
          this.innerHTML = 'O';
+         document.getElementById('playTurn').innerHTML = "Player 2's Turn (O)";
          arr[idx] = 2;
       } else {
          this.innerHTML = 'X';
+         document.getElementById('playTurn').innerHTML = "Player 1's Turn (X)";
          arr[idx] = 1;
       }
       checkWin();
@@ -74,12 +73,22 @@ function marker() {
 }
 
 function setBoard() {
-   // var element = document.createElement(tagName[, options]);
+
+   app.innerHTML = "";
+   arr = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+   numberClicks = 1;
+   status = "";
+
    var title = document.createElement('p');
    title.innerHTML = "Tic-Tac-Toe";
-   title.className = "h1 text-center";
+   title.className = "h1 text-center p-4";
    app.appendChild(title);
 
+   var playerTurn = document.createElement('p');
+   playerTurn.innerHTML = "Player 1's Turn (X)";
+   playerTurn.className = "h3 text-center";
+   playerTurn.id = "playTurn";
+   app.appendChild(playerTurn);
 
    // Create Board Container
    var newDiv = document.createElement('div');
@@ -98,5 +107,12 @@ function setBoard() {
       newDiv.appendChild(rowDiv);
    }
    app.appendChild(newDiv);
+
+   var resetBtn = document.createElement('p');
+   resetBtn.innerHTML = "Reset";
+   resetBtn.className = "btn btn-dark btn-lg m-4 col-6";
+   resetBtn.addEventListener('click', setBoard);
+   app.appendChild(resetBtn);
 }
+
 
